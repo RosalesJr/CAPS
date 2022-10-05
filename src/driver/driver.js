5d9631f27447d06a22b2dfbe87ab61eca19ec371
LUIS I ROSALES JR <luis.rosalesjr01@gmail.com>
2022-10-05 00:34:17 +0000

    refactored code to have connections between emits

--- src/driver/driver.js
+++ src/driver/driver.js
@@ -3,7 +3,12 @@
 let eventPool = require('../eventPool');
 
 module.exports = (payload) => {
-  console.log(`order has been picked up ${payload.order.orderId}`);
-  console.log('order is on the way', payload);
-  console.log(`order is delivered ${payload.order.orderId}`);
+
+  setTimeout(() => {
+    console.log(`order has been picked up ${payload.order.orderId}`);
+    console.log('order is on the way', payload);
+    eventPool.emit('TRANSIT', payload);
+
+  }, 3000);
 };
+
