4de5f58af047bbe47c79de795937d59b6acc2d2c
LUIS I ROSALES JR <luis.rosalesjr01@gmail.com>
2022-10-06 18:17:52 +0000

    refactored code to include order id and correct store name

--- src/driver/index.js
+++ src/driver/index.js
@@ -6,5 +6,7 @@ const driver = new messageClient('driver');
 driver.publish('GET_ALL',  { queueId: 'orderId'});
 
 driver.subscribe('PICKUP', payload => {
+  console.log('picked up delivery', payload.order.orderId );
   driver.publish('DELIVERED', payload);
+  console.log('order is delivered', payload.order.orderId);
 });
